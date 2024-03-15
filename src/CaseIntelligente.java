6d7adaad2758552f4933f6e523ef81dd54be48cf
Kylian <kylian.dumas@etu.univ-orleans.fr>
2024-03-15 10:51:55 +0100

    casei

--- src/CaseIntelligente.java
+++ src/CaseIntelligente.java
@@ -1,10 +1,23 @@
+import java.util.List;
+
 public class CaseIntelligente extends Case{
-    private Plateau lePlateau;
+    private List<Case> lesVoisines;
 
     public CaseIntelligente(){
         super();
     }
     public void ajouteVoisine(Case uneCase){
-        
+        lesVoisines.add(uneCase);
     }
+    public int nbBombesVoisines(){
+        int res = 0;
+        for(Case elt:lesVoisines){
+            if(elt.contientBombe){res+=1;}}
+        return res;
+    }
+    public String toString(){
+        if(this.estDecouverte()==false && this.estMarque()==false){return " ";}
+        else if(this.estMarque()){return "?";}
+        else if(this.contientBombe()){return "@";}
+        else{return String.valueOf(this.nbBombesVoisines());}}
 }
